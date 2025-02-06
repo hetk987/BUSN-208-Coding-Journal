f509773e153119116a00d634e6a39fa102ecb043
hetk987 <66132321+hetk987@users.noreply.github.com>
2025-02-05 23:47:31 -0600

    validate the input of the user

--- Week 3/TicTacToe.playground/Contents.swift
+++ Week 3/TicTacToe.playground/Contents.swift
@@ -31,9 +31,13 @@ class TicTacToe{
         //num_move <= max_moves
         while (num_move < max_moves){
             printBoard()
-            print(currPlayer.name + "\'s turn!")
-            print("Place a piece")
-                    
+            print("\(currPlayer.name) 's turn!")
+            print("Place an \(currPlayer.chip.rawValue)")
+            let cord = getValidMove()
+            if cord != nil {
+                
+            }
+            
         }
         
         //enter where they want to play (row, col)
@@ -43,11 +47,27 @@ class TicTacToe{
         //players can store their moves
     }
     
-    func printBoard(){
+    func printBoard()->Void{
         print("-----")
         for i in board {
-            print(i.joined(separator: " | "))
+            print(i.joined(separator: " | ")) //Combined the list elements with the separator
             print("-----")
         }
     }
+    
+    func getValidMove() -> (Int, Int)?{
+        print("Enter row (0-2) and column (0-2) separated by space:")
+                guard let input = readLine()?.split(separator: " ").compactMap({ Int($0) }), //Reads the line from the terminal and then splits it into a list and then converts it to a n int. The guard allows me to have a bunch of checks on it to make sure it is valid. 
+                      input.count == 2,
+                      input[0] < 3,
+                      input[1] < 3,
+                      input[0] > 0,
+                      input[1] > 0,
+                      board[input[0]][input[1]] == " " else {
+                    print("Invalid move. Try again.")
+                    return nil
+                }
+                return (input[0], input[1])
+    }
 }
+
