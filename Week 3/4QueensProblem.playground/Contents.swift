d318697aa2c2318c2d5cae7b3d93507789f8d87f
hetk987 <66132321+hetk987@users.noreply.github.com>
2025-02-06 13:07:27 -0600

    Added a better function that can also check the diagonals

--- Week 3/4QueensProblem.playground/Contents.swift
+++ Week 3/4QueensProblem.playground/Contents.swift
@@ -1,37 +1,62 @@
 var board = Array(repeating: Array(repeating: " ", count: 4), count: 4)
 
-func isValidQueen(row:Int, col:Int, board:[[String]]) -> Bool{
-    var n:Int = board.count
+func isValidQueen(row: Int, col: Int, board: [[String]]) -> Bool {
+    let n = board.count
     var count = 0
 
     // Check horizontal
-    for i in 0...n {
+    for i in 0..<n {
         if board[row][i] == "Q" {
             count += 1
         }
     }
     
     // Check vertical
-    for i in 0...n {
+    for i in 0..<n {
         if board[i][col] == "Q" {
             count += 1
         }
     }
     
-    // Diagonal: right to left
-    for i in 0...(n-col) {
-        if board[i][col] == "Q" {
+    // Diagonal: top-left to bottom-right
+    var i = row, j = col
+    while i >= 0 && j >= 0 {
+        if board[i][j] == "Q" {
             count += 1
         }
+        i -= 1
+        j -= 1
     }
-    
-    // Diagonal: left to right
-    for i in 0...(n-col) {
-        if board[i][col] == "Q" {
+    i = row + 1
+    j = col + 1
+    while i < n && j < n {
+        if board[i][j] == "Q" {
             count += 1
         }
+        i += 1
+        j += 1
     }
     
+    // Diagonal: top-right to bottom-left
+    i = row
+    j = col
+    while i >= 0 && j < n {
+        if board[i][j] == "Q" {
+            count += 1
+        }
+        i -= 1
+        j += 1
+    }
+    i = row + 1
+    j = col - 1
+    while i < n && j >= 0 {
+        if board[i][j] == "Q" {
+            count += 1
+        }
+        i += 1
+        j -= 1
+    }
     
-    return count <= 4
+    return count == 4 // The queen counts itself in all four checks
 }
+
