01a9f472d2863ec9784576cda0bebcc492a77e9d
hetk987 <66132321+hetk987@users.noreply.github.com>
2025-02-17 21:12:17 -0600

    added a vstack and images

--- Week 4/Carfax frontend/Carfax frontend/ContentView.swift
+++ Week 4/Carfax frontend/Carfax frontend/ContentView.swift
@@ -17,7 +17,22 @@ struct CaravanView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20) {
                     ForEach(items, id: \.self) { item in
-                        
+                        VStack {
+                            Image(item) // Use your own custom images
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fit)
+//                                .frame(width: 200, height: 200)
+//                                .cornerRadius(15)
+//                                .shadow(radius: 5)
+//                            Text(item)
+//                                .font(.headline)
+//                                .foregroundColor(.primary)
+                        }
+//                        .padding()
+//                        .background(Color.white)
+//                        .cornerRadius(20)
+//                        .shadow(radius: 10)
+                    }
                 }
                 .padding()
             }
